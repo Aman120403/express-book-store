65144dc35c6dec540cc077263280178384be133d
Aman120403 <amanmaurya904463@gmail.com>
2026-03-05 16:51:27 +0530

    update project

---
+++ routes/book.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {BOOKS} = require('../models/book');
+const router = express.Router();
+const controller = require("../controllers/book.controller")
+
+router.get('/',controller.getAllBooks);
+
+router.get('/:id',controller.getBookById);
+
+router.post('/',controller.createBook);
+
+router.delete('/:id', controller.deleteBook);
+
+module.exports = router;
